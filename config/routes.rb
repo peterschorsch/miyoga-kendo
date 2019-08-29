cb788f130686d4a4ba917b60971043a28705ccc8
Peter <peteschorsch@gmail.com>
2019-08-29 12:36:39 -0500

    Added Contact Us section

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
 
   get :practices, to: 'practices#index', path: 'class-schedule'
+  get :contacts, to: 'contacts#index', path: 'contact-us'
 
   namespace :admin do
     resources :users
