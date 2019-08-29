ee265b0b929c8363bfdb093b13208b71e9785fc4
Peter <peteschorsch@gmail.com>
2019-08-29 12:43:30 -0500

    Added About section

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'home', to: 'home#index'
 
+  get :abouts, to: 'abouts#index', path: 'about'
   get :practices, to: 'practices#index', path: 'class-schedule'
   get :contacts, to: 'contacts#index', path: 'contact-us'
 
