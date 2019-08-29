952c5fbc974274d1e1ab13c0614a232954b3d629
Peter <peteschorsch@gmail.com>
2019-08-29 13:02:07 -0500

    Added Helpful Links section

---
+++ app/controllers/helpful_links_controller.rb
@@ -0,0 +1,5 @@
+class HelpfulLinksController < ApplicationController
+	def index
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get :abouts, to: 'abouts#index', path: 'about'
   get :practices, to: 'practices#index', path: 'class-schedule'
   get :contacts, to: 'contacts#index', path: 'contact-us'
+  get :helpful_links, to: 'helpful_links#index', path: 'helpful-links'
 
   namespace :admin do
     resources :users
