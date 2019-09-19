1e62d169271efd276dce5055271f62e51ee6d998
Peter Schorsch <peterschogmail.comm>
2019-09-18 17:27:58 -0700

    Added 3 fake admins and users to seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
 start = Time.now
-puts "Started seeding"
-@admin_user = User.find_or_create_by(firstname: "Test", lastname: "Admin", email: "testadmin@gmail.com", password: BCrypt::Password.create("testadmin"), role: "Admin")
+puts "STARTED SEEDING"
+puts ""
+
+### CREATE TEST USERS ###
+puts "CREATE TEST USERS"
+
+## ADMINS ##
+puts "ADMINS"
+@admin_user = User.create_with(firstname: "Peter", lastname: "Schorsch", active: true, password: BCrypt::Password.create("peteschorsch"), role: "Admin", last_login: DateTime.now-1.week).find_or_create_by(email: "peteschorsch@gmail.com")
 puts @admin_user.inspect
-@user = User.find_or_create_by(firstname: "Test", lastname: "User", email: "testuser@gmail.com", password: BCrypt::Password.create("testuser"), role: "User")
+@admin_user = User.create_with(firstname: "Jason", lastname: "Toy", active: true, password: BCrypt::Password.create("jasontoy"), role: "Admin", last_login: DateTime.now-1.week).find_or_create_by(email: "jasontoy@icloud.com")
+puts @admin_user.inspect
+@admin_user = User.create_with(firstname: "Vince", lastname: "Chan", active: true, password: BCrypt::Password.create("vincechan"), role: "Admin", last_login: DateTime.now-1.week).find_or_create_by(email: "vincechan@yahoo.com")
+puts @admin_user.inspect
+puts ""
+
+puts "USERS"
+## USERS ##
+@user = User.create_with(firstname: "John", lastname: "Smith", active: true, password: BCrypt::Password.create("johnsmith"), role: "User", last_login: DateTime.now-2.weeks).find_or_create_by(email: "jsmith@gmail.com")
 puts @user.inspect
-puts "Ran seeding in " + (Time.now - start).to_s + " seconds"
+
+@user = User.create_with(firstname: "Donald", lastname: "Long", active: true, password: BCrypt::Password.create("donaldlong"), role: "User", last_login: DateTime.now-2.weeks).find_or_create_by(email: "dlong@yahoo.com")
+puts @user.inspect
+
+@user = User.create_with(firstname: "Tom", lastname: "Jones", active: true, password: BCrypt::Password.create("tomjones"), role: "User", last_login: DateTime.now-2.weeks).find_or_create_by(email: "tjones@msnglobal.net")
+puts @user.inspect
+
+puts ""
+puts "RAN SEEDING IN " + (Time.now - start).to_s + " SECONDS"
