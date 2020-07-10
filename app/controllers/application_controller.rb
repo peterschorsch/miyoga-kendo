384eefe1630539b019c728db208131877c523887
Peter Schorsch <peterschogmail.comm>
2020-07-10 15:43:25 -0700

    Redesigned home page to match initial mockups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 	helper_method :current_user
 	
+
 	def current_user
 		if session[:user_id]
 			@current_user ||= User.find(session[:user_id])
 		else
 			@current_user = nil
 		end
+	end
+
+	def landing_page
+		@disable_body = true
 	end
 
 end
