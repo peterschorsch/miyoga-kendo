1d08a8cf30a599a9b848174b5c26e664e19a434d
Peter Schorsch <peteschorsch@gmail.com>
2020-08-20 11:03:35 -0700

    Added new content button to guest side of resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,14 +2,17 @@ class ResourcesController < ApplicationController
 	before_action :set_page
 
 	def index
-		@all_resources_content = @page.contents.includes(:links)
+		@all_resources_content = @current_page.contents.includes(:links)
 		@resources = @all_resources_content.display_non_articles
 
 		@articles = @all_resources_content.display_articles
+
+		@new_resource = Content.new
+		@new_resource.links.build
 	end
 
 	private
 	def set_page
-		@page = Page.named("Resources")
+		@current_page = Page.named("Resources")
 	end
 end
