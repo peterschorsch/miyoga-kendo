5d2d5fae32556319dde68bf8715bbc8b4f77d501
Peter Schorsch <peteschorsch@gmail.com>
2021-02-22 13:06:21 -0800

    Added path helper for templates except for nested-forms

---
+++ app/helpers/path_helper.rb
@@ -0,0 +1,69 @@
+module PathHelper
+
+	def admin_notice_path
+		"templates/alerts/admin-notice"
+	end
+
+	def alert_path
+		"templates/alerts/alert"
+	end
+
+	def form_errors_path
+		"templates/alerts/form-errors"
+	end
+
+	def notice_path
+		"templates/alerts/notice"
+	end
+
+	def back_to_page_path
+		"templates/buttons/back-to-page"
+	end
+
+	def back_to_admin_home_path
+		"templates/buttons/back-to-admin-home"
+	end
+
+	def new_btn_path
+		"templates/buttons/crud/new"
+	end
+
+	def new_btn_tag_path
+		"templates/buttons/crud/new-button-tag"
+	end
+
+	def edit_btn_path
+		"templates/buttons/crud/edit"
+	end
+
+	def edit_btn_tag_path
+		"templates/buttons/crud/edit-button-tag"
+	end
+
+	def remove_btn_tag_path
+		"templates/buttons/crud/remove-button-tag"
+	end
+
+	def remove_btn_path
+		"templates/buttons/crud/remove"
+	end
+
+	def show_btn_path
+		"templates/buttons/crud/show"
+	end
+
+
+	def remove_record_dialog
+		"templates/modals/remove-record-dialog"
+	end
+
+	def google_maps_path
+		"templates/google_maps"
+	end
+
+	def heading_path
+		"templates/heading"
+	end
+
+
+end
