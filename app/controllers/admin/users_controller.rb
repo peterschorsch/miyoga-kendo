ab5bc0097826df9007f01e3df56b2328b7df1944
Peter Schorsch <peteschorsch@gmail.com>
2021-02-24 12:59:19 -0800

    Tweaked User form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,6 @@
 class Admin::UsersController < Admin::AdminController
   before_action :set_user, only: [:show, :edit, :update, :update_password, :reset_token]
 
-  # GET /admin/users
-  # GET /admin/users.json
   def index
     @users = User.select(:id, :firstname, :lastname, :email, :role, :active, :last_login).remove_guest_account
     @active_users = @users.user_accounts.active_accounts.order_by_name
@@ -11,22 +9,16 @@ class Admin::UsersController < Admin::AdminController
     @inactive_admins = @users.admin_accounts.archived_accounts.order_by_name
   end
 
-  # GET /admin/users/1
-  # GET /admin/users/1.json
   def show
   end
 
-  # GET /admin/users/new
   def new
     @user = User.new
   end
 
-  # GET /admin/users/1/edit
   def edit
   end
 
-  # POST /admin/users
-  # POST /admin/users.json
   def create
     @user = User.new(user_params)
 
@@ -45,12 +37,10 @@ class Admin::UsersController < Admin::AdminController
     end
   end
 
-  # PATCH/PUT /admin/users/1
-  # PATCH/PUT /admin/users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to admin_users_path, notice: "#{@user.concat_name} was successfully updated." }
+        format.html { redirect_to request.referrer, notice: "#{@user.concat_name} was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
