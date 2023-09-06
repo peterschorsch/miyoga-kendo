ded6e37e294df71a4ac9e2c0108dd5582532caec
Peter Schorsch <peter.c.schorsch@gmail.com>
2023-09-06 15:46:16 -0700

    Ensured admins can only edit thier account

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::AdminController
+  before_action :authorized?, only: [:show, :edit, :update, :update_password, :reset_token]
   before_action :set_user, only: [:show, :edit, :update, :update_password, :reset_token]
 
   def index
@@ -101,5 +102,13 @@ class Admin::UsersController < Admin::AdminController
     def dojo_address_params
       # NOTE: Using 'strong_parameters' gem
       params.require(:address).permit(:id, :location_name, :address_line_1, :address_line_2, :city, :state_id, :zip_code, :notes, :user_id)
+    end
+    def authorized?
+    user = User.find(params[:id] || params[:user][:id])
+    ### IF CURRENT USER IS EXISTS || USER DOESN'T MATCH CURRENT USER || IS NOT AN ADMIN, ARCHIVED OR A GUEST
+    if current_user.blank? || current_user != user || !(current_user.is_admin? || current_user.is_archived? || current_user.is_guest?)
+        flash[:alert] = "You are not authorized to do said action."
+        redirect_to admin_users_path
+      end
     end
 end
